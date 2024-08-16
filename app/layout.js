80ac218c4d7920c589d0d67effdcef254edbb47d
Syed Taha <syetaha@gmail.com>
2024-08-17 01:35:59 +0500

    refactor: Update app layout to use @vercel/analytics/react for analytics

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Analytics } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
